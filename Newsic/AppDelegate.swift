c9fef757cb085925130a954ca2fb66b9767b3a35
Bryan's Air <battiste1313@gmail.com>
2018-11-07 19:03:12 -0700

    feat rearrange storyboard

--- Newsic/AppDelegate.swift
+++ Newsic/AppDelegate.swift
@@ -17,11 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         dataController.load()
-        
-        let navigationController = window?.rootViewController as! UINavigationController
-        let NewsTableViewController = navigationController.topViewController as! NewsTableViewController
-        NewsTableViewController.dataController = dataController
-        
+                
         return true
     }
     
