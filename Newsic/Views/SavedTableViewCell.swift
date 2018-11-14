47c1af06610a1e78b514246bb837942f940bb54e
Bryan's Air <battiste1313@gmail.com>
2018-11-14 11:20:05 -0700

    feat add activity indicator to image cell saveView before downloading

--- Newsic/Views/SavedTableViewCell.swift
+++ Newsic/Views/SavedTableViewCell.swift
@@ -14,7 +14,9 @@ class SavedTableViewCell: UITableViewCell {
     @IBOutlet weak var cellLabel: UILabel!
     @IBOutlet weak var cellDateLabel: UILabel!
     @IBOutlet weak var cellSourceLabel: UILabel!
-        
+    @IBOutlet weak var cellActivityIndicatorImage: UIActivityIndicatorView!
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -23,7 +25,7 @@ class SavedTableViewCell: UITableViewCell {
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
-        // Configure the view for the selected state
+        cellActivityIndicatorImage.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
     }
 
 }
