414e105a5bd6d876e4ab35e621d58b7725cf2908
Bryan's Air <battiste1313@gmail.com>
2018-12-03 11:28:04 -0700

    feat change status bar to .light to see in dark backgrounds of view

--- Newsic/ViewControllers/SearchViewController.swift
+++ Newsic/ViewControllers/SearchViewController.swift
@@ -29,6 +29,10 @@ class SearchViewController: UIViewController {
 //------------------------------------------------------------------------------
 // MARK: Lifecyle
     
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
